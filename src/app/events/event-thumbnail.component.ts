d048333788b44164a9b976d1c05710b9f860ce88
Danilo Ventura <danilo.ventura@scai-consulting.grupposcai.it>
2021-05-22 17:40:44 +0200

    Lesson 4.5

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,8 +5,9 @@ import {  Component,  Input  } from '@angular/core';
   selector: 'event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styles: [`
+      .thumbnail { min-height: 210px; }
       .pad-left {margin-left: 10px; } 
-      .well div {color: #bbb}     
+      .well div {color: #bbb}   
       `
   ]
 })
